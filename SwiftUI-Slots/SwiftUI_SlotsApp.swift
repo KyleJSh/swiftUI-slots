f6d6474cc665ebbd01958c4fd72c836d3b475afd
Kyle Sherrington <kylejsherrington@gmail.com>
2021-01-23 16:56:08 -0800

    Initial Commit

---
+++ SwiftUI-Slots/SwiftUI_SlotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_SlotsApp.swift
+//  SwiftUI-Slots
+//
+//  Created by Kyle Sherrington on 2021-01-23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_SlotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
